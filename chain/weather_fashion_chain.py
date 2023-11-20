157881b08e77f16b0e21b1bc1ba5210c8c36d33e
hsolemio-lee <hslyjy@gmail.com>
2023-11-20 09:21:41 +0900

    Update weather_fashion_chain.py

--- chain/weather_fashion_chain.py
+++ chain/weather_fashion_chain.py
@@ -4,18 +4,15 @@ from langchain.chains import LLMChain
 
 from output_parsers import weather_summary_parser, fashion_style_parser
 
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-1106", timeout=30)
 def get_coordinates_chain() -> LLMChain:
     summary_template = """
         given the region {region_name}, I want you to get it me longitude and latitude of the given region.
         Your answer should be only a number and format have to be json format and each value need to be float.
     """
-
     summary_prompt_template = PromptTemplate(
         input_variables=["region_name"],
-        template=summary_template
+        template=summary_template,
     )
 
     return LLMChain(llm=llm, prompt=summary_prompt_template)
@@ -25,7 +22,7 @@ def get_weather_chain() -> LLMChain:
         given the weather information {information}, I want you to create:
         1. current temperature
         2. weather in a word
-        3. Recommend fashion styles following temperature and weather with top, bottom, shoes, outerwear and accessory as a string.
+        3. Recommend fashion styles following temperature and weather with top, bottom, shoes, outerwear and accessory.
         
         \n{format_instructions}
     """
@@ -43,7 +40,7 @@ def get_weather_chain() -> LLMChain:
 
 def get_fashion_image_chain() -> LLMChain:
     image_template = """
-        With below image description, give me only url including the following 
+        With below image description, make the url following 
         https://image.pollinations.ai/prompt/[description]), where [description] = {region_name},%20[adjective1],%20[charactersDetailed],%20[adjective2],%20[visualStyle1],%20[visualStyle2],%20[visualStyle3],%20[genre],%20[artistReference]
         Description : 
         I want to get a geek and nerdy style fashion with below clothes only for male. 
